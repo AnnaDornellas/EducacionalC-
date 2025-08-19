00792fbe7f1c736951c4c7dfce011a4af45a6f54
AnnaJDornellas <anna.ds00123@gmail.com>
2025-08-18 21:05:48 -0300

    aula 2

--- Academico/Models/Aluno.cs
+++ Academico/Models/Aluno.cs
@@ -1,11 +1,13 @@
-﻿namespace Academico.Models
+﻿using System.ComponentModel;
+
+namespace Academico.Models
 {
     public class Aluno
     {
         public int Nome { get; set; }
-
+        [DisplayName("e-mail")]
         public int Email { get; set; }
-
+        [DisplayName("CEP")]
         public int Cep { get; set; }
 
 
